b653bda9f9e0ff32d9534d8359dfb851e69892d9
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-07-21 23:09:28 +0530

    21-07

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,26 @@
-
 import React from "react";
-import vpn from '../assets/vpn.png'
-import foodie from '../assets/foodie.jpg'
-import Footer from './Footer'
+import dance from '../assets/dance.jpg';
+import foodie from '../assets/foodie.jpg';
+import Footer from './Footer';
 
 const ProjectCard = ({ image, title, description, git, technologies }) => {
     return (
         <div className="max-w-sm sm:max-w-sm md:max-w-sm bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            {title=='Snap Shot' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={vpn} alt="" />
-            </a>}
-            {title=='Co People' && <a href="#">
-                <img className="w-full rounded-t-lg h-auto object-cover " src={foodie} alt="" />
-            </a>}
+            <a href="#">
+                <img className="w-full rounded-t-lg h-auto object-cover" src={image} alt={title} />
+            </a>
             <div className="p-4 sm:p-6">
                 <a href="#">
                     <h5 className="text-2xl sm:text-xl md:text-2xl lg:text-3xl font-bold tracking-tight text-white bg-clip-text text-transparent bg-gradient-to-r from-yellow-200 to-pink-500">{title}</h5>
                 </a>
                 <p className="font-normal text-sm sm:text-base md:text-lg text-gray-300 dark:text-gray-400">{description}</p>
             </div>
-            <div className='m-2 sm:m-4 lg:m-6 flex justify-between'>
-                <div className='flex flex-wrap gap-2 pl-2'>
+            <div className="m-2 sm:m-4 lg:m-6 flex justify-between">
+                <div className="flex flex-wrap gap-2 pl-2">
                     {technologies.map((tag, index) => (
                         <p
                             key={`${index}-${tag}`}
-                            className='text-[14px] text-blue-500'
+                            className="text-[14px] text-blue-500"
                         >
                             #{tag}
                         </p>
@@ -35,7 +31,7 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
         </div>
     );
 };
-  
+
 const Projects = () => {
     return (
         <div className="bg-black">
@@ -46,33 +42,31 @@ const Projects = () => {
                         image={item.image}
                         title={item.title}
                         description={item.description}
-                        links={item.links}
                         git={item.git}
                         technologies={item.technologies}
                     />
                 ))}
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
-}
-
+};
 
 export const project = [
     {
-        title:'Co People',
-        description:'Co People is a dynamic web application I crafted using React, Node JS and React. This project is a modern and engaging social platform that allows users to connect, share content and interact seamlessly.',
-        image: {vpn},
-        git:'https://github.com/santoshsaroj0032/Food-Delivery-Application',
-        technologies:['MongoDb' ,'ReactJS' , 'NodeJS']
+        title: 'Food Delivery Application',
+        description: 'Co People is a dynamic web application I crafted using React, Node JS and React. This project is a modern and engaging social platform that allows users to connect, share content and interact seamlessly.',
+        image: foodie,
+        git: 'https://github.com/santoshsaroj0032/Food-Delivery-Application',
+        technologies: ['MongoDb', 'ReactJS', 'NodeJS']
     },
     {
-        title:'Snap Shot',
-        description:'SnapShot is a stunning portfolio that I exclusively designed using React JS and tailwind CSS.This Project serves as a representation of a photographer’s work, highlighting their portfolio and services.',
-        image: {foodie},
-        git:"https://github.com/nithingooud/vpn_studios",
-        technologies:[ 'React JS', 'tailwind CSS']
+        title: 'Dance Academy',
+        description: 'SnapShot is a stunning portfolio that I exclusively designed using React JS and tailwind CSS. This Project serves as a representation of a photographer’s work, highlighting their portfolio and services.',
+        image: dance,
+        git: "https://github.com/santoshsaroj0032/DanceAcademy",
+        technologies: ['NodeJs', 'ExpressJs', 'MongoDb', 'PUG', 'CSS']
     }
-]
+];
 
-export default Projects
+export default Projects;
