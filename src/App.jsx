50209bc31225fcedc19f3af30f25c564a485c64f
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-09-07 21:46:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+ import { BrowserRouter ,Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
